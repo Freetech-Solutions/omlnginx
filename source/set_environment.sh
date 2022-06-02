9aa78ccf7b384a2a99f80b0bc64ef37abc60082b
jjlorenzo <jjlorenzo@gmail.com>
2022-06-02 16:27:33 +0000

    OML-2177

--- source/set_environment.sh
+++ source/set_environment.sh
@@ -39,8 +39,10 @@ location ~ ^/(channels) {
 EOF
 elif [ $ENV == "prodenv" ]; then
   if [ $INFRA == "docker" ]; then
+    DAPHNE_URL="proxy_pass http://app:8098;"
     UWSGI_PASS="  uwsgi_pass         app:8099;"
   elif [ $INFRA == "onpremise" ]; then
+    DAPHNE_URL="proxy_pass http://unix:/opt/omnileads/run/oml_daphne.socket;"
     UWSGI_PASS="  uwsgi_pass      unix:/opt/omnileads/run/oml_uwsgi.socket;"
     sed -i "s/alias \/var\/spool\/asterisk\/monitor.*/alias \/opt\/omnileads\/asterisk\/var\/spool\/asterisk\/monitor;/g" /etc/nginx/conf.d/ominicontacto.conf
   fi
@@ -64,18 +66,14 @@ location / {
 }
 
 location ~ ^/(channels) {
-  ${UWSGI_PASS}
-  include         uwsgi_params;
-  uwsgi_send_timeout 600s;
-  uwsgi_read_timeout 600s;
-  uwsgi_connect_timeout 600s;
+  ${DAPHNE_URL}
   keepalive_timeout 600s;
   send_timeout      600s;
-  uwsgi_param HTTP_X_REAL_IP \$remote_addr;
-  uwsgi_param HTTP_X_FORWARDED_FOR \$proxy_add_x_forwarded_for;
-  uwsgi_param HTTP_X_FORWARDED_PORT \$server_port;
-  uwsgi_param HTTP_X_FORWARDED_PROTO \$scheme;
-  uwsgi_param HTTP_REFERER \$http_referer;
+  proxy_set_header X-Real-IP \$remote_addr;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Port \$server_port;
+  proxy_set_header X-Forwarded-Proto \$scheme;
+  proxy_set_header Referer 	     \$http_referer;
   proxy_read_timeout 600s;
   proxy_connect_timeout 600s;
   proxy_send_timeout 600s;
