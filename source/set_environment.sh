91996a7ae8d5d98eefd62c4f1b5b16b5700d9660
fpignataro <2620878-psychodoom@users.noreply.gitlab.com>
2024-01-08 21:17:32 +0000

    Oml 455 dev nginx services envvar ports

--- source/set_environment.sh
+++ source/set_environment.sh
@@ -13,7 +13,7 @@ location / {
   proxy_set_header X-Forwarded-Port \$server_port;
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_set_header Referer 	     \$http_referer;
-  proxy_pass http://${DJANGO_HOSTNAME}:8099;
+  proxy_pass http://${DJANGO_HOSTNAME}:${WSGI_PORT};
   proxy_read_timeout 600s;
   proxy_connect_timeout 600s;
   proxy_send_timeout 600s;
@@ -26,7 +26,7 @@ location ~ ^/(channels) {
   proxy_set_header X-Forwarded-Port \$server_port;
   proxy_set_header X-Forwarded-Proto \$scheme;
   proxy_set_header Referer 	     \$http_referer;
-  proxy_pass http://${DAPHNE_HOSTNAME}:8098;
+  proxy_pass http://${DAPHNE_HOSTNAME}:${ASGI_PORT};
   proxy_read_timeout 600s;
   proxy_connect_timeout 600s;
   proxy_send_timeout 600s;
@@ -38,7 +38,7 @@ location ~ ^/(channels) {
 
 location ~* (ws) {
   alias /opt/omnileads/static/ominicontacto/JS/socket.io.js;
-  proxy_pass https://${KAMAILIO_HOSTNAME}:14443;
+  proxy_pass https://${KAMAILIO_HOSTNAME}:${KAMAILIO_PORT};
   proxy_http_version 1.1;
   proxy_set_header Upgrade \$http_upgrade;
   proxy_set_header Connection "upgrade";
@@ -53,7 +53,7 @@ location ~* (ws) {
 
 location /consumers {
   alias /opt/omnileads/static/ominicontacto/JS/socket.io.js;
-  proxy_pass http://${WEBSOCKETS_HOSTNAME}:8000;
+  proxy_pass http://${WEBSOCKETS_HOSTNAME}:${WEBSOCKETS_PORT};
   proxy_http_version 1.1;
   proxy_set_header Upgrade \$http_upgrade;
   proxy_set_header Connection "upgrade";
