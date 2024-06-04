1ee148ca49f2facd57a1ea5f06ff24192ffb9935
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-04 17:14:29 -0300

    added the skeleton of the endpoints

--- interface/app.py
+++ interface/app.py
@@ -5,5 +5,31 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route('/create-campaign')
+def create_campaign():
+    return "Creating campaign ..."
+
+
+@app.route('/stop-campaign')
+def stop_campaign():
+    return "Stopping campaign ..."
+
+
+@app.route('/pause-campaign')
+def pause_campaign():
+    return "Pausing campaign ..."
+
+
+@app.route('/resume-campaign')
+def resume_campaign():
+    return "Resuming campaign ..."
+
+
+@app.route('/delete-campaign')
+def delete_campaign():
+    return "Removing campaign ..."
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1440, debug=True)
