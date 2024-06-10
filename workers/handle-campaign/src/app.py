25e0b27b6007c841f60dac7df9889bf10308ddf4
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-10 15:23:18 -0300

    added latest worker's skeletons

--- workers/handle-campaign/src/app.py
+++ workers/handle-campaign/src/app.py
@@ -9,6 +9,8 @@ gm_worker.register_task(b'stop-campaign', lambda gearman_worker, gearman_job: b'
 gm_worker.register_task(b'pause-campaign', lambda gearman_worker, gearman_job: b'Campaign paused!')
 gm_worker.register_task(b'resume-campaign', lambda gearman_worker, gearman_job: b'Campaign resumed!')
 gm_worker.register_task(b'delete-campaign', lambda gearman_worker, gearman_job: b'Campaign deleted!')
-gm_worker.register_task(b'process-campaign', lambda gearman_worker, gearman_job: b'Campaign is being processed')
+gm_worker.register_task(b'process-campaign', lambda gearman_worker, gearman_job: b'Campaign is being processed ...')
+gm_worker.register_task(b'call-contact', lambda gearman_worker, gearman_job: b'Contact is being called ... ')
+gm_worker.register_task(b'syncronize-campaign', lambda gearman_worker, gearman_job: b'Campaign is being synchronized ...')
 
 gm_worker.work()
