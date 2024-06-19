97f8cf951e5f413895db63d11459a4b2e9f70f13
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-19 00:04:18 -0300

    added the types of contact

--- interface/src/dialer/multichannel.py
+++ interface/src/dialer/multichannel.py
@@ -10,16 +10,24 @@ from .basic import Dialer
 from settings.default import GEARMAN_JOB_SERVERS
 
 
+class ContactTypes:
+    PHONE = 1
+    EMAIL = 2
+    SMS = 3
+    WHATSAPP = 4
+    CUSTOM_BY_CONTACT = 5
+
+
 class MultiChannelDialer(Dialer):
     """A dialer design to make multi-channel contacts"""
 
 
-    gm_client = gearman.GearmanClient(GEARMAN_JOB_SERVERS)
+    GM_CLIENT = gearman.GearmanClient(GEARMAN_JOB_SERVERS)
 
 
     @classmethod
     def create_campaign(cls, id_campaign):
-        job_request = cls.gm_client.submit_job('create-campaign', id_campaign)
+        job_request = cls.GM_CLIENT.submit_job('create-campaign', id_campaign)
         return job_request.result
 
 
