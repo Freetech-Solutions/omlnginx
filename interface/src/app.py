51d142ead5123b213d14ff3612dbead22835cd71
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-20 14:18:22 -0300

    - made flexible the choice of the dialer class

--- interface/src/app.py
+++ interface/src/app.py
@@ -13,44 +13,46 @@ app = Flask(__name__)
 
 app.config.from_object('settings.default')
 
+DIALER = MultiChannelDialer
+
 # TODO: pass a parameter called 'type' for dispatch to
 # the class linked to that kind of a campaing (voip, email, Whatsapp, Telegram, SMS, etc)
 
 @app.route('/create-campaign/<id_campaign>', methods = ['POST'])
 def create_campaign(id_campaign):
     strategy = request.get_json().get('contact-strategy', [])
-    return MultiChannelDialer.create_campaign(id_campaign, strategy)
+    return DIALER.create_campaign(id_campaign, strategy)
 
 
 @app.route('/edit-campaign/<id_campaign>', methods = ['POST'])
 def edit_campaign(id_campaign):
     strategy = request.get_json().get('contact-strategy', [])
-    return MultiChannelDialer.edit_campaign(id_campaign, strategy)
+    return DIALER.edit_campaign(id_campaign, strategy)
 
 
 @app.route('/start-campaign/<id_campaign>', methods = ['POST'])
 def start_campaign():
-    return MultiChannelDialer.start_campaign(id_campaign)
+    return DIALER.start_campaign(id_campaign)
 
 
 @app.route('/stop-campaign/<id_campaign>', methods = ['POST'])
 def stop_campaign():
-    return MultiChannelDialer.stop_campaign(id_campaign)
+    return DIALER.stop_campaign(id_campaign)
 
 
 @app.route('/pause-campaign/<id_campaign>', methods = ['POST'])
 def pause_campaign():
-    return MultiChannelDialer.pause_campaign(id_campaign)
+    return DIALER.pause_campaign(id_campaign)
 
 
 @app.route('/resume-campaign/<id_campaign>', methods = ['POST'])
 def resume_campaign():
-    return MultiChannelDialer.resume_campaign(id_campaign)
+    return DIALER.resume_campaign(id_campaign)
 
 
 @app.route('/delete-campaign/<id_campaign>', methods = ['POST'])
 def delete_campaign():
-    return MultiChannelDialer.delete_campaign(id_campaign)
+    return DIALER.delete_campaign(id_campaign)
 
 
 if __name__ == '__main__':
