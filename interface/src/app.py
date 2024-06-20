64f568256210d535cf969f77f21392f3f0a0641b
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-20 11:52:54 -0300

    the endpoints should be POST not GET

--- interface/src/app.py
+++ interface/src/app.py
@@ -16,32 +16,32 @@ app.config.from_object('settings.default')
 # TODO: pass a parameter called 'type' for dispatch to
 # the class linked to that kind of a campaing (voip, email, Whatsapp, Telegram, SMS, etc)
 
-@app.route('/create-campaign/<id_campaign>')
+@app.route('/create-campaign/<id_campaign>', methods = ['POST'])
 def create_campaign(id_campaign):
     return MultiChannelDialer.create_campaign(id_campaign)
 
 
-@app.route('/start-campaign/<id_campaign>')
+@app.route('/start-campaign/<id_campaign>', methods = ['POST'])
 def start_campaign():
     return MultiChannelDialer.start_campaign(id_campaign)
 
 
-@app.route('/stop-campaign/<id_campaign>')
+@app.route('/stop-campaign/<id_campaign>', methods = ['POST'])
 def stop_campaign():
     return MultiChannelDialer.stop_campaign(id_campaign)
 
 
-@app.route('/pause-campaign/<id_campaign>')
+@app.route('/pause-campaign/<id_campaign>', methods = ['POST'])
 def pause_campaign():
     return MultiChannelDialer.pause_campaign(id_campaign)
 
 
-@app.route('/resume-campaign/<id_campaign>')
+@app.route('/resume-campaign/<id_campaign>', methods = ['POST'])
 def resume_campaign():
     return MultiChannelDialer.resume_campaign(id_campaign)
 
 
-@app.route('/delete-campaign/<id_campaign>')
+@app.route('/delete-campaign/<id_campaign>', methods = ['POST'])
 def delete_campaign():
     return MultiChannelDialer.delete_campaign(id_campaign)
 
