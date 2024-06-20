17b254b8854c898aa373861c19a7e84db562e9d8
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-20 15:36:31 -0300

    - removed some responsabilities from interface classes to add it to workers

--- interface/src/dialer/basic.py
+++ interface/src/dialer/basic.py
@@ -1,41 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Dialer:
-    """The general process of a dialer, in an abstract way"""
-
-
-    @classmethod
-    def process_campaign(cls, id_campaign):
-        while cls.campaign_is_active(id_campaign):
-            contacts_attempts_number = cls.allowed_parallel_contact_attempts(id_campaign)
-            for contact in cls.take_contacts(contacts_attempts_number, id_campaign):
-                cls.attempt_contact(contact, id_campaign)
-
-
-    @classmethod
-    def attempt_contact(cls, contact):
-        pass
-
-
-    @classmethod
-    def campaign_is_active(cls, id_campaign):
-        pass
-
-
-    @classmethod
-    def allowed_parallel_contact_attempts(cls, id_campaign):
-        pass
-
-
-    @classmethod
-    def take_contacts(cls, contacts_attempts_number, id_campaign):
-        pass
-
-
-    @classmethod
-    def attempt_contact(cls, contact, id_campaign):
-        pass
-
+    """The general features of a dialer, in an abstract way"""
 
     @classmethod
     def create_campaign(cls, id_campaign, contact_strategy):
--- interface/src/dialer/multichannel.py
+++ interface/src/dialer/multichannel.py
@@ -10,14 +10,6 @@ from .basic import Dialer
 from settings.default import GEARMAN_JOB_SERVERS
 
 
-class ContactTypes:
-    PHONE = 1
-    EMAIL = 2
-    SMS = 3
-    WHATSAPP = 4
-    CUSTOM_BY_CONTACT = 5
-
-
 class MultiChannelDialer(Dialer):
     """A dialer design to make multi-channel contacts"""
 
@@ -40,25 +32,3 @@ class MultiChannelDialer(Dialer):
     def start_campaign(cls, id_campaign):
         job_request = cls.GM_CLIENT.submit_job('start-campaign', id_campaign)
         return job_request.result
-
-
-    @classmethod
-    def attempt_contact(cls, contact, id_campaign):
-        contact_type_order = cls.get_contact_type_order(id_campaign)
-        channel = cls.get_channel_to_contact(contact_type_order, contact)
-        return cls.attempt_contact_channel(contact, channel)
-
-
-    @classmethod
-    def get_contact_type_order(cls, id_campaign):
-        pass
-
-
-    @classmethod
-    def get_channel_to_contact(cls, contact_type_order, contact):
-        pass
-
-
-    @classmethod
-    def attempt_contact_channel(cls, contact, channel):
-        pass
