582e9c20010cf2474cf22f8adf332fab59762ce6
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-22 17:30:52 -0300

    partial commit

--- workers/handle-campaign/src/handler/dummy.py
+++ workers/handle-campaign/src/handler/dummy.py
@@ -6,12 +6,30 @@ from .basic import DialerWorker
 
 from random import randrange
 
+import requests
+
+import json
+
+import sys
+
+import os
+
+
+ASTERISK_USER = os.getenv('ARI_USER', 'default_user')
+ASTERISK_PASS = os.getenv('ARI_PASS', 'default_pass')
+ASTERISK_HOST = os.getenv('ARI_HOST', 'asterisk')
+ASTERISK_PORT = os.getenv('ARI_PORT', '7088')
+ASTERISK_APP = os.getenv('ASTERISK_APP', 'call_manager')
+ARI_BASE_URL = f'http://{ASTERISK_HOST}:{ASTERISK_PORT}/ari'
+
+
 
 class DummyWorker(DialerWorker):
     """A worker flow just for testing stuff, mocking a lot ..."""
 
 
     CAMPAIGN_ACTIVE_COUNTER = 0
+
 
     CAMPAIGN_ACTIVE_MAX_ITERATIONS = 7
 
@@ -56,4 +74,29 @@ class DummyWorker(DialerWorker):
     @classmethod
     def attempt_contact(cls, contact, id_campaign):
         sleep(1)                # just for distinguish the background job
+
+        phone_number = contact
+        id_customer = 7777777
+        queue_timeout = 0
+        dial_timeout = 0
+        channel_type = 'pstn_dialout'
+
+
+        call_data = {
+            'endpoint': f'PJSIP/{tel_number}@TroncalSIP0',
+            'callerId': '01177660010',
+            'timout': 15,
+            'app': ASTERISK_APP,
+            'appArgs': f'id_camp: {id_campaign}, id_customer: {id_customer}, tel_customer: {tel_number}, queue_timeout: {queue_timeout}, channel_type: {channel_type}'
+        }
+
+        # Realiza la solicitud para crear un nuevo canal (originate)
+        response = requests.post(
+            f'{ari_base_url}/channels',
+            auth=(ASTERISK_USER, ASTERISK_PASS),
+            headers={'Content-Type': 'application/json'},
+            data=json.dumps(call_data)
+        )
+
+
         print('Calling contact {0} in campaign {1}'.format(contact, id_campaign))
