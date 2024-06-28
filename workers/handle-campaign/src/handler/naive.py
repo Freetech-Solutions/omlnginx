0d8c46798785de296b1b7cf16a121c6f8741eeb7
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-06-28 14:51:28 -0300

    - implemented 'campaign_is_active' method

--- workers/handle-campaign/src/handler/naive.py
+++ workers/handle-campaign/src/handler/naive.py
@@ -97,13 +97,14 @@ class NaiveWorker(DialerWorker):
     @classmethod
     def start_campaign(cls, job):
         id_campaign = int(job.data)
+        cls.REDIS_DIALER_CONNECTION.hset(f'OML:CAMPAIGN:{if_campaign}', 'status', 'active')
         cls.process_campaign(id_campaign)
         return b'Campaign started!'
 
 
     @classmethod
     def campaign_is_active(cls, id_campaign):
-        pass
+        return cls.REDIS_DIALER_CONNECTION.hget(f'OML:CAMPAIGN:{id_campaign}', 'status') == 'active'
 
 
     @classmethod
