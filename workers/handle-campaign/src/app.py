262b29ea1d3a2b51799f1c7171171ccbf5a81ada
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-07-08 15:09:03 -0300

    minor change

--- workers/handle-campaign/src/app.py
+++ workers/handle-campaign/src/app.py
@@ -15,9 +15,8 @@ gm_worker.register_task(b'stop-campaign', lambda gearman_worker, gearman_job: b'
 gm_worker.register_task(b'pause-campaign', lambda gearman_worker, gearman_job: WORKER.pause_campaign(gearman_job))
 gm_worker.register_task(b'resume-campaign', lambda gearman_worker, gearman_job: WORKER.resume_campaign(gearman_job))
 gm_worker.register_task(b'delete-campaign', lambda gearman_worker, gearman_job: b'Campaign deleted!')
-gm_worker.register_task(b'process-campaign', lambda gearman_worker, gearman_job: b'Campaign is being processed ...')
-gm_worker.register_task(b'call-contact', lambda gearman_worker, gearman_job: b'Contact is being called ... ')
+gm_worker.register_task(b'process-contact', lambda gearman_worker, gearman_job: WORKER.process_contact(gearman_job))
 gm_worker.register_task(b'syncronize-campaign', lambda gearman_worker, gearman_job: b'Campaign is being synchronized ...')
-gm_worker.register_task(b'process-ari-event', lambda gearman_worker, gearman_job: WORKER.process_ari_event(gearman_job))
+gm_worker.register_task(b'process-event', lambda gearman_worker, gearman_job: WORKER.process_event(gearman_job))
 
 gm_worker.work()
