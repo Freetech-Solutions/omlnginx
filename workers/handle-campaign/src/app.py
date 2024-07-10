6f3a84da4ad2fc940b414b2a1762233709f51a17
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-07-10 17:49:52 -0300

    - classifying the Gearman processes

--- workers/handle-campaign/src/app.py
+++ workers/handle-campaign/src/app.py
@@ -8,15 +8,21 @@ WORKER = NaiveWorker
 
 gm_worker = gearman.GearmanWorker(GEARMAN_JOB_SERVERS)
 
-gm_worker.register_task(b'create-campaign', lambda gearman_worker, gearman_job: WORKER.create_campaign(gearman_job))
+# long processes
 gm_worker.register_task(b'start-campaign', lambda gearman_worker, gearman_job: WORKER.start_campaign(gearman_job))
+gm_worker.register_task(b'resume-campaign', lambda gearman_worker, gearman_job: WORKER.resume_campaign(gearman_job))
+gm_worker.register_task(b'process-contact', lambda gearman_worker, gearman_job: WORKER.process_contact(gearman_job))
+gm_worker.register_task(b'process-event', lambda gearman_worker, gearman_job: WORKER.process_event(gearman_job))
+
+# short processes
+gm_worker.register_task(b'create-campaign', lambda gearman_worker, gearman_job: WORKER.create_campaign(gearman_job))
 gm_worker.register_task(b'edit-campaign', lambda gearman_worker, gearman_job: b'Campaign modified!')
 gm_worker.register_task(b'stop-campaign', lambda gearman_worker, gearman_job: b'Campaign stopped!')
 gm_worker.register_task(b'pause-campaign', lambda gearman_worker, gearman_job: WORKER.pause_campaign(gearman_job))
-gm_worker.register_task(b'resume-campaign', lambda gearman_worker, gearman_job: WORKER.resume_campaign(gearman_job))
 gm_worker.register_task(b'delete-campaign', lambda gearman_worker, gearman_job: b'Campaign deleted!')
-gm_worker.register_task(b'process-contact', lambda gearman_worker, gearman_job: WORKER.process_contact(gearman_job))
+
+# scheduled processes
 gm_worker.register_task(b'syncronize-campaign', lambda gearman_worker, gearman_job: b'Campaign is being synchronized ...')
-gm_worker.register_task(b'process-event', lambda gearman_worker, gearman_job: WORKER.process_event(gearman_job))
+
 
 gm_worker.work()
