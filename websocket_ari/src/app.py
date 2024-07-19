0983fd11d426e840e986eda3ee2c6fb7ac6d244b
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-07-19 09:52:05 -0300

    updated config

--- websocket_ari/src/app.py
+++ websocket_ari/src/app.py
@@ -38,7 +38,7 @@ class CallManager:
         """
 
         self.ari_host =  os.getenv('ASTERISK_HOST', 'acd')
-        self.ari_port =  os.getenv('ASTERISK_PORT', '7088')
+        self.ari_port =  os.getenv('ASTERISK_PORT', '8888')
         self.ari_user =  os.getenv('ASTERISK_USER', 'omnileads')
         self.ari_password =  os.getenv('ASTERISK_PASS', '5_MeO_DMT')
 
