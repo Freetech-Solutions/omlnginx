a7308d08547c18499faa3ada919bd6986f48f65d
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-09-10 12:07:12 -0300

    fix

--- workers/handle-campaign/src/settings/default.py
+++ workers/handle-campaign/src/settings/default.py
@@ -2,8 +2,8 @@ import os
 
 GEARMAN_JOB_SERVERS = os.getenv('GEARMAN_JOB_SERVERS').split('|')
 
-REDIS_DIALER_SERVER = os.getenv(REDIS_DIALER_SERVER, 'omnidialer-redis')
+REDIS_DIALER_SERVER = os.getenv('REDIS_DIALER_SERVER', 'omnidialer-redis')
 
-REDIS_DIALER_PORT = os.getenv(REDIS_DIALER_PORT, 6380)
+REDIS_DIALER_PORT = os.getenv('REDIS_DIALER_PORT', 6380)
 
 GEARMAN_JOBS = os.getenv('GEARMAN_JOBS').split('|')
