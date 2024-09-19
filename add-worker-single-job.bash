3ec03a77ea1e5e631781956435d76022a2300b70
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-09-19 15:30:55 -0300

    - added tags for dedicated workers

--- add-worker-single-job.bash
+++ add-worker-single-job.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -itd --env-file .env -e GEARMAN_JOBS=$1 --network=omnileads_omnileads omnidialer_worker
+docker run --rm -itd --name=$2 --env-file .env -e GEARMAN_JOBS=$1 --network=omnileads_omnileads omnidialer_worker
--- start-dedicated.bash
+++ start-dedicated.bash
@@ -3,13 +3,13 @@
 docker-compose up -d
 
 for i in {1..3}; do
-    bash add-worker-single-job.bash process-contact
+    bash add-worker-single-job.bash process-contact process-contact-$i
 done
 
-bash add-worker-single-job.bash create-campaign
+bash add-worker-single-job.bash create-campaign create-campaign-1
 
-bash add-worker-single-job.bash process-event
+bash add-worker-single-job.bash process-event process-event-1
 
-bash add-worker-single-job.bash schedule-contact
+bash add-worker-single-job.bash schedule-contact schedule-contact-1
 
-bash add-worker-single-job.bash send-reports
+bash add-worker-single-job.bash send-reports send-reports-1
