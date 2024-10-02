ad389d87e768a222be3aa8ea2f0d53c43be8ac0d
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-10-02 14:34:28 -0300

    - remove redundant invocation

--- rebuild-testing-create.bash
+++ rebuild-testing-create.bash
@@ -2,6 +2,4 @@
 
 bash rebuild-clean.bash
 
-docker exec -it omnidialer-redis redis-cli -p 6380 flushdb
-
 curl -d '{"contact-strategy": [1, 3, 4]}' -H "Content-Type: application/json" -X POST http://localhost:1440/create-campaign/4
