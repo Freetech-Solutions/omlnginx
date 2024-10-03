c253baf1cedb9a5036478af7738cc2b7f94d74ae
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-10-03 15:27:50 -0300

    improved testing script

--- rebuild-testing-create.bash
+++ rebuild-testing-create.bash
@@ -3,3 +3,9 @@
 bash rebuild-clean.bash
 
 curl -d '{"contact-strategy": [1, 3, 4]}' -H "Content-Type: application/json" -X POST http://localhost:1440/create-campaign/4
+
+docker exec -it dialer-postgres psql -U omnidialer -c "insert into incidence_rules values (1, 1, 'busy', 4, 20, 1, 4);"
+
+docker exec -it dialer-postgres psql -U omnidialer -c "insert into incidence_rules values (2, 4, 'congestion', 3, 40, 1, 4)";
+
+docker exec -it dialer-postgres psql -U omnidialer -c "update campaign set hour_start = '00:00', hour_ends = '23:59', monday = True, tuesday = True, wednesday = True, thursday = True, friday = True, end_date = '2025-10-29' where id = 4;"
