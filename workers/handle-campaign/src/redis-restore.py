ac0d3c28c387f6f351416d6c4149679b0164b2fa
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-10-11 01:35:46 -0300

    partial commit: working on Redis restore script

--- workers/handle-campaign/src/redis-restore.py
+++ workers/handle-campaign/src/redis-restore.py
@@ -2,6 +2,8 @@ from handler.naive import AverageWorker
 
 import os
 
+import pickle
+
 import json
 
 import logging
@@ -22,20 +24,23 @@ def restore_redis_from_postgres():
     with psycopg.connect(AverageWorker.POSTGRES_DIALER_CONNECTION_STR) as conn_dialer:
         cursor_dialer = conn_dialer.cursor()
         size = 1000
-        cursor_dialer.execute('select history, id_contact, id_campaign, status from only contact_in_campaign;')
+        cursor_dialer.execute('select history, id_contact, id_campaign, status, phone_numbers_list, phone_number_index from only contact_in_campaign;')
         while True:
             contacts = cursor_dialer.fetchmany(size=size)
             if not contacts:
                 break
             logger.debug('Importing {0} contacts from Postgres to Redis'.format(len(contacts)))
-            for (history, id_contact, id_campaign, status) in contacts:
+            for (history, id_contact, id_campaign, status, phone_numbers, phone_number_index) in contacts:
                 for status_item in history:
                     AverageWorker.REDIS_DIALER_CONNECTION.rpush(f'CONTACT:{id_contact}:CAMP:{id_campaign}:HISTORY', status_item)
+                AverageWorker.REDIS_DIALER_CONNECTION.hset(f'CONTACT:{id_contact}:CAMP:{id_campaign}', 'PHONE_NUMBER_LIST', pickle.dumps(phone_numbers))
+                AverageWorker.REDIS_DIALER_CONNECTION.hset(f'CONTACT:{id_contact}:CAMP:{id_campaign}', 'PHONE_NUMBER_INDEX', pickle.dumps(phone_number_index))
                 AverageWorker.REDIS_DIALER_CONNECTION.hset(f'CONTACT:{id_contact}:CAMP:{id_campaign}', 'STATUS', status)
         cursor_dialer.execute('select id, statistics from only campaign;')
         for id_campaign, statistics in cursor_dialer.fetchall():
             for report, counter in statistics.items():
                 AverageWorker.REDIS_DIALER_CONNECTION.hset(f'CAMP:{id_campaign}:COUNTER', report, counter)
+
 
 
 
