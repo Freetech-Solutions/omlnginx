70a3aebd25261edf4cf30a7e56fa803310c6e244
Alejandro Zamora <Alejandro Zamora Fonseca>
2024-10-14 12:53:32 -0300

    made congestion by default use multinum in script rebuild-testing-truncated.bash

--- rebuild-testing-truncated.bash
+++ rebuild-testing-truncated.bash
@@ -6,7 +6,7 @@ curl -d '{"contact-strategy": [1, 3, 4]}' -H "Content-Type: application/json" -X
 
 docker exec -it dialer-postgres psql -U omnidialer -c "insert into incidence_rules values (1, 1, 'busy', 4, 20, 1, 4);"
 
-docker exec -it dialer-postgres psql -U omnidialer -c "insert into incidence_rules values (2, 4, 'congestion', 3, 40, 1, 4)";
+docker exec -it dialer-postgres psql -U omnidialer -c "insert into incidence_rules values (2, 4, 'congestion', 3, 40, 2, 4)";
 
 docker exec -it dialer-postgres psql -U omnidialer -c "update campaign set hour_start = '00:00', hour_ends = '23:59', monday = True, tuesday = True, wednesday = True, thursday = True, friday = True, end_date = '2025-10-29' where id = 4;"
 
