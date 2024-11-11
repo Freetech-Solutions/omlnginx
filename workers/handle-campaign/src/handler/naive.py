284fab31acfe6837afc6880a966dde720675c9bc
Alejandro Zamora <ale2014.zamora@gmail.com>
2024-11-11 00:08:23 -0300

    pep8 stuff

--- workers/handle-campaign/src/handler/naive.py
+++ workers/handle-campaign/src/handler/naive.py
@@ -1026,10 +1026,11 @@ class AverageWorker(DialerWorker):
         with psycopg.connect(cls.POSTGRES_DIALER_CONNECTION_STR) as conn_dialer:
             cursor_dialer = conn_dialer.cursor()
             cursor_dialer.execute(
-                """INSERT INTO incidence_rules (id, status, status_custom, max_attempt, retry_later, mode, campaign_id) VALUES
-                (%s, %s, %s, %s, %s, %s, %s);""", (id_rule, status, status_custom, max_attempt, retry_later, mode, id_campaign))
+                """INSERT INTO incidence_rules (id, status, status_custom, max_attempt, retry_later,
+                mode, campaign_id) VALUES
+                (%s, %s, %s, %s, %s, %s, %s);""",
+                (id_rule, status, status_custom, max_attempt, retry_later, mode, id_campaign))
         return b'Incidence rule was added'
-
 
 
 class SingleCallWorker(AverageWorker):
