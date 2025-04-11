9195a2088b230a575c713f3227a1e9a2950296f2
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-04-11 17:23:38 -0300

    - working in unit tests for the latest changes - added skeleton and made refactor for a better setUp phase

--- workers/handle-campaign/src/tests.py
+++ workers/handle-campaign/src/tests.py
@@ -39,6 +39,9 @@ class MyTestSuite(unittest.TestCase):
             cursor_dialer = conn_dialer.cursor()
             cursor_dialer.execute('DELETE FROM campaign;')
             cursor_dialer.execute('DELETE FROM contact;')
+            cursor_dialer.execute('DELETE FROM incidence_rules;')
+            cursor_dialer.execute('DELETE FROM incidence_rules_disposition;')
+            cursor_dialer.execute('DELETE FROM contact_in_campaign;')
         AverageWorker.REDIS_DIALER_CONNECTION.close()
 
     def mocked_psycopg_fetchmany(self, cursor, size):
@@ -57,13 +60,11 @@ class MyTestSuite(unittest.TestCase):
             return MagicMock()
         return self.ORIGINAL_PSYCOPG_CONNECT(connection_str)
 
-    def test_handle_campaign(self):
-        # TODO1: isolate it to a multiple unit tests
-        # TODO2: add more unit tests
+    def _create_campaign(self):
         # mocking Postgres connection to OML
         psycopg.connect = MagicMock(side_effect=self.mocked_psycopg_connect)
         # mocking get_campaign_data
-        campaign_id_data = (
+        self.campaign_id_data = (
             4, 2, 'test_dialer_01', datetime.date(2024, 8, 21),
             datetime.date(2024, 8, 21), 2, 10, 'rrmemory', 10, False, Decimal('1.0'), 1, False,
             True, False, False, False, False, False, datetime.time(15, 51), datetime.time(15, 51),
@@ -71,18 +72,50 @@ class MyTestSuite(unittest.TestCase):
             '"{\\"prim_fila_enc\\": false, \\"cant_col\\": 6, \\"nombres_de_columnas\\": '
             '[\\"telefono\\", \\"nombre\\", \\"apellido\\", \\"dni\\", \\"telefono2\\", '
             '\\"telefono3\\"], \\"cols_telefono\\": [0, 4, 5]}"', '0')
-        incidence_rules_data = [(1, 1, 'busy', 4, 20, 1, 4), (2, 4, 'congestion', 3, 40, 1, 4)]
-        incidence_rules_disposition_data = [(1, 7, 3, 17, 1, 4), (2, 8, 5, 7, 2, 4)]
-        campaign_mocked_data = (campaign_id_data, incidence_rules_data,
-                                incidence_rules_disposition_data)
+        self.incidence_rules_data = [(1, 1, 'busy', 4, 20, 1, 4), (2, 4, 'congestion', 3, 40, 1, 4)]
+        self.incidence_rules_disposition_data = [(1, 7, 3, 17, 1, 4), (2, 8, 5, 7, 2, 4)]
+        campaign_mocked_data = (self.campaign_id_data, self.incidence_rules_data,
+                                self.incidence_rules_disposition_data)
         AverageWorker.get_campaign_data = MagicMock(
             return_value=campaign_mocked_data)
         AverageWorker.process_campaign = MagicMock()
         AverageWorker.get_contacts_campaign = MagicMock(side_effect=self.mocked_psycopg_fetchmany)
-        worker = GearmanWorker()
+        self.worker = GearmanWorker()
         job = GearmanJob(None, None, None, None,
                          b'{"id_campaign": "4", "contact_strategy": [1, 3, 4]}')
-        AverageWorker.create_campaign(worker, job)
+        AverageWorker.create_campaign(self.worker, job)
+
+    def test_clean_broken_selected_contacts_starting_campaing(self):
+        # mark one contact to SELECT_CALL status
+        # run start_campaign
+        # ensure the contact has now CREATED status
+        pass
+
+    def test_clean_broken_selected_contacts_resuming_campaing(self):
+        # mark one contact to SELECT_CALL status
+        # run resume_campaign
+        # ensure the contact has now CREATED status
+        pass
+
+    def test_incidence_rules(self):
+        # make sure if an event came to process event and there is an incidence rule attached to it
+        # it will schedule a call if the contact has still a valid number of attempts
+        pass
+
+    def test_incidence_rules_disposition(self):
+        # make sure if a disposition came to the disposition endpoint abd there is an incidence rule
+        # disposition attached to it  will schedule a call if the contact has still a valid number
+        # of attempts
+        pass
+
+    def test_call_is_tagged_as_aborted_if_campaign_not_active(self):
+        pass
+
+    def test_campaign_is_paused_if_expired(self):
+        pass
+
+    def test_handle_campaign_general(self):
+        self._create_campaign()
         # check campaign entry creation and related tables too
         with psycopg.connect(AverageWorker.POSTGRES_DIALER_CONNECTION_STR) as conn_dialer:
             cursor_dialer = conn_dialer.cursor()
@@ -100,18 +133,18 @@ class MyTestSuite(unittest.TestCase):
         # let's edit the campaign now
         job = GearmanJob(None, None, None, None,
                          b'{"id_campaign": "4", "contact_strategy": [1, 4]}')
-        campaign_id_data = campaign_id_data[:-4] + ([1, 4],) + campaign_id_data[-3:]
-        campaign_mocked_data = (campaign_id_data, incidence_rules_data,
-                                incidence_rules_disposition_data)
+        campaign_id_data = self.campaign_id_data[:-4] + ([1, 4],) + self.campaign_id_data[-3:]
+        campaign_mocked_data = (campaign_id_data, self.incidence_rules_data,
+                                self.incidence_rules_disposition_data)
         AverageWorker.get_campaign_data = MagicMock(
             return_value=campaign_mocked_data)
-        AverageWorker.edit_campaign(worker, job)
+        AverageWorker.edit_campaign(self.worker, job)
         # let's add an incidence rule
         job = GearmanJob(
             None, None, None, None,
             b'{"id_campaign": 4, "id_rule": 3, "status": 3, "status_custom":"no answer", '
             b'"max_attempt": 5, "retry_later": 5, "mode": 1, "type_rule": 1}')
-        AverageWorker.create_incidence_rule(worker, job)
+        AverageWorker.create_incidence_rule(self.worker, job)
 
         with psycopg.connect(AverageWorker.POSTGRES_DIALER_CONNECTION_STR) as conn_dialer:
             AverageWorker.GM_CLIENT.submit_job = MagicMock()
@@ -125,7 +158,7 @@ class MyTestSuite(unittest.TestCase):
                 None, None, None, None,
                 b'{"id_campaign": 4, "id_rule": 3, "status": 3, "status_custom":"no answer", '
                 b'"max_attempt": 7, "retry_later": 5, "mode": 1, "type_rule": 1}')
-            AverageWorker.update_incidence_rule(worker, job)
+            AverageWorker.update_incidence_rule(self.worker, job)
             id_rule = 3
             cursor_dialer.execute(
                 'SELECT max_attempt FROM ONLY incidence_rules WHERE id = %s;',
@@ -136,7 +169,7 @@ class MyTestSuite(unittest.TestCase):
             job = GearmanJob(
                 None, None, None, None,
                 b'{"id_campaign": 4, "id_rule": 3, "type_rule": 1}')
-            AverageWorker.delete_incidence_rule(worker, job)
+            AverageWorker.delete_incidence_rule(self.worker, job)
             cursor_dialer.execute('SELECT COUNT(*) FROM ONLY incidence_rules;')
             self.assertEqual(cursor_dialer.fetchone()[0], 2)
             id_campaign = campaign_id_data[0]
@@ -148,19 +181,19 @@ class MyTestSuite(unittest.TestCase):
             }
             payload_bytes = self.encode_payload(payload)
             job = GearmanJob(None, None, None, None, payload_bytes)
-            AverageWorker.start_campaign(worker, job)
+            AverageWorker.start_campaign(self.worker, job)
             status_campaign = AverageWorker.get_campaign_status(id_campaign, cursor_dialer)
             self.assertEqual(status_campaign, ACTIVE)
 
             # testing pause-campaign
             job = GearmanJob(None, None, None, None, payload_bytes)
-            AverageWorker.pause_campaign(worker, job)
+            AverageWorker.pause_campaign(self.worker, job)
             status_campaign = AverageWorker.get_campaign_status(id_campaign, cursor_dialer)
             self.assertEqual(status_campaign, PAUSED)
 
             # testing resume-campaign
             job = GearmanJob(None, None, None, None, payload_bytes)
-            AverageWorker.resume_campaign(worker, job)
+            AverageWorker.resume_campaign(self.worker, job)
             status_campaign = AverageWorker.get_campaign_status(id_campaign, cursor_dialer)
             self.assertEqual(status_campaign, ACTIVE)
 
@@ -169,16 +202,16 @@ class MyTestSuite(unittest.TestCase):
                 None, None, None, None,
                 b'{"id_campaign": "4", "disposition_option": 8, "id_contact": 1}')
             for i in range(5):
-                AverageWorker.add_incidence_rule_disposition(worker, job)
+                AverageWorker.add_incidence_rule_disposition(self.worker, job)
                 self.assertTrue(AverageWorker.GM_CLIENT.submit_job.called)
                 AverageWorker.GM_CLIENT.submit_job.reset_mock()
-            AverageWorker.add_incidence_rule_disposition(worker, job)
+            AverageWorker.add_incidence_rule_disposition(self.worker, job)
             self.assertFalse(AverageWorker.GM_CLIENT.submit_job.called)
             AverageWorker.GM_CLIENT.submit_job.reset_mock()
 
             # testing stop-campaign
             job = GearmanJob(None, None, None, None, payload_bytes)
-            AverageWorker.stop_campaign(worker, job)
+            AverageWorker.stop_campaign(self.worker, job)
             status_campaign = AverageWorker.get_campaign_status(id_campaign, cursor_dialer)
             self.assertEqual(status_campaign, FINALIZED)
 
