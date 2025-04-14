8c3a22626049f7019cabc58646e8582ed8726025
Alejandro Zamora <Alejandro Zamora Fonseca>
2025-04-14 14:38:49 -0300

    improved unit tests script

--- run-tests.bash
+++ run-tests.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --build --remove-orphans
+if [ -z "$1" ]
+then docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --build --remove-orphans
+else docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --remove-orphans
+fi
 
 docker exec -it omnidialer-worker-test python -m unittest tests.py
 
