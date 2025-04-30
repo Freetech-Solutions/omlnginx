2e6f979e789a09b423b2d7259d1492c493c4c698
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-04-30 00:20:30 -0300

    - fix to the issue of selected contacts for call not being called - added TODO

--- workers/handle-campaign/src/handler/naive.py
+++ workers/handle-campaign/src/handler/naive.py
@@ -194,23 +194,25 @@ class AverageWorker(DialerWorker):
                 caps_calls_counter = 0
                 contacts = cls.take_contacts(contacts_attempts_number, id_campaign)
                 for contact in contacts:
-                    current_time = datetime.datetime.now()
-                    current_delta = current_time - initial_time
-                    if current_delta >= timedelta(seconds=1):
-                        logger.debug(f"Campaign {id_campaign}: CAPS init")
-                        caps_calls_counter = 0
-                        initial_time = current_time
-                    else:
-                        if caps_calls_counter < CAPS:
-                            logger.debug(
-                                f"Campaign {id_campaign}: attempt to call selected contacts")
-                            # TODO: mark contacts as SELECTED_CALL
-                            cls.attempt_contact(contact, id_campaign)
-                            caps_calls_counter += 1
+                    while True:
+                        # we need to ensure the selected contact is eventually called
+                        current_time = datetime.datetime.now()
+                        current_delta = current_time - initial_time
+                        if current_delta >= timedelta(seconds=1):
+                            logger.debug(f"Campaign {id_campaign}: CAPS init")
+                            caps_calls_counter = 0
+                            initial_time = current_time
                         else:
-                            logger.debug(f"Campaign {id_campaign}: CAPS sleep")
-                            remaining = (timedelta(seconds=1) - current_delta).total_seconds()
-                            sleep(remaining)
+                            if caps_calls_counter < CAPS:
+                                logger.debug(
+                                    f"Campaign {id_campaign}: attempt to call selected contact")
+                                cls.attempt_contact(contact, id_campaign)
+                                caps_calls_counter += 1
+                                break
+                            else:
+                                logger.debug(f"Campaign {id_campaign}: CAPS sleep")
+                                remaining = (timedelta(seconds=1) - current_delta).total_seconds()
+                                sleep(remaining)
 
     @classmethod
     def connect_redis_oml(cls):
@@ -717,6 +719,7 @@ class AverageWorker(DialerWorker):
     @classmethod
     def attempt_contact(cls, contact, id_campaign):
         message = json.dumps({'contact': contact, 'id_campaign': id_campaign})
+        # TODO: think if the following could be a background job call
         cls.GM_CLIENT.submit_job('process-contact', message)
 
     @classmethod
