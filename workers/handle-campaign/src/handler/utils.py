75b33498c32c3bc32292f52d36f0e5ef32ba901a
Alejandro Zamora <Alejandro Zamora Fonseca>
2025-05-22 15:10:15 -0300

    - fixed warnings in code after upgrade to 3.12.8 - fork of gearman library and modification to run under python 3.12.8 - fixes to requirements & Dockerfiles to use the modified library - updated to python3.12 - upgraded all images to python 3.12.8

--- workers/handle-campaign/src/handler/utils.py
+++ workers/handle-campaign/src/handler/utils.py
@@ -1,18 +1,18 @@
 from functools import lru_cache, wraps
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 
 def timed_lru_cache(seconds: int, maxsize: int = 128):
     def wrapper_cache(func):
         func = lru_cache(maxsize=maxsize)(func)
         func.lifetime = timedelta(seconds=seconds)
-        func.expiration = datetime.utcnow() + func.lifetime
+        func.expiration = datetime.now(UTC) + func.lifetime
 
         @wraps(func)
         def wrapped_func(*args, **kwargs):
-            if datetime.utcnow() >= func.expiration:
+            if datetime.now(UTC) >= func.expiration:
                 func.cache_clear()
-                func.expiration = datetime.utcnow() + func.lifetime
+                func.expiration = datetime.now(UTC) + func.lifetime
 
             return func(*args, **kwargs)
 
