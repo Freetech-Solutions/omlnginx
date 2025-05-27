cbb18b030539df04303f4e5b7c1bef281cf2e41d
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-05-27 19:41:42 -0300

    fixed PEP8 stuff

--- workers/handle-campaign/src/handler/naive.py
+++ workers/handle-campaign/src/handler/naive.py
@@ -961,7 +961,8 @@ class AverageWorker(DialerWorker):
                 phone_number = cls.get_next_phone_number(
                     cursor_dialer, id_campaign, contact_id, phone_number)
                 retry_later = cls.get_delay(retry_later, type_incidence_rule, attempt_number)
-                datetime_retry_later = datetime.datetime.now(datetime.UTC) + timedelta(seconds=retry_later)
+                datetime_retry_later = datetime.datetime.now(datetime.UTC) + timedelta(
+                    seconds=retry_later)
                 message = json.dumps({'id_campaign': str(id_campaign),
                                       'id_contact': contact_id,
                                       'phone_number': phone_number,
